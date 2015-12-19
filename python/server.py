80da227e2614cdca69939c106b7542204ae3ff13
Phil Eaton <philneaton95@gmail.com>
2015-12-18 20:55:40 -0500

    adds python implementation

---
+++ python/server.py
@@ -0,0 +1,31 @@
+import atexit
+import socket
+
+
+def main():
+    sock = socket.socket()
+    sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, True)
+    sock.bind(("", 9222))
+    sock.listen(-1)
+
+    atexit.register(lambda: sock.shutdown(socket.SHUT_RDWR))
+
+    while True:
+        conn, address = sock.accept()
+        conn.settimeout(2)
+        data = ""
+        while True:
+            try:
+                _data = conn.recv(1024)
+            except socket.timeout:
+                break
+
+            data += _data.decode("utf-8")
+
+        conn.sendall("Hello world".encode("utf-8"))
+
+        conn.close()
+
+
+if __name__ == "__main__":
+    main()
